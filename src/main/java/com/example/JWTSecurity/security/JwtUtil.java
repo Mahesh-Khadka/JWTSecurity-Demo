9684787fe1809b730cfe096ccbcb427777f889b1
Mahesh-Khadka <maheshkhadka438@gmail.com>
2025-09-20 18:59:43 +0545

    Modify the JwtUtil so that SecretKey and Expiration can set in application.properties

--- src/main/java/com/example/JWTSecurity/security/JwtUtil.java
+++ src/main/java/com/example/JWTSecurity/security/JwtUtil.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -11,19 +13,22 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.expiration}")
+    private long EXPIRATION;
 
     private Key getSigningKey(){
-        String SECRET_KEY = "mysecretkeymysecretkeymysecretkey";
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 
     public String generateToken(String email){
-        long EXPIRATION = 1000 * 60 * 60;
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(
-                        new Date(System.currentTimeMillis() + EXPIRATION)
+                        new Date(System.currentTimeMillis()+ EXPIRATION)
                 )
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();

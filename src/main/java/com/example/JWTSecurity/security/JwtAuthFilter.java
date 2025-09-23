302c38b6533627671ac29cfc474f1bee6aba15b1
Mahesh-Khadka <maheshkhadka438@gmail.com>
2025-09-23 20:27:31 +0545

    feat(JwtAuthFilter):validate JWT and set authenticated user in securityContext from JWT

--- src/main/java/com/example/JWTSecurity/security/JwtAuthFilter.java
+++ src/main/java/com/example/JWTSecurity/security/JwtAuthFilter.java
@@ -1,12 +1,21 @@
+package com.example.JWTSecurity.security;
+
+import com.example.JWTSecurity.entity.User;
 import com.example.JWTSecurity.repository.UserRepository;
 import com.example.JWTSecurity.security.JwtUtil;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -20,9 +29,38 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
   @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws SecurityException, IOException{
+      throws ServletException, IOException{
       final String authHeader = request.getHeader("Authorization");
       String email = null;
       String token = null;
+
+      if(authHeader != null && authHeader.startsWith("Bearer ")){
+          token = authHeader.substring(7);
+          email = jwtUtil.extractEmail(token);
+      }
+
+      if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+          if (jwtUtil.validateToken(token)) {
+              // Fetch user from database to get role
+              User user = userRepository.findByEmail(email).orElse(null);
+
+              if (user != null) {
+                  UsernamePasswordAuthenticationToken authToken =
+                          new UsernamePasswordAuthenticationToken(
+                                  user,
+                                  null,
+                                  List.of(new SimpleGrantedAuthority(user.getRole())) // add role
+                          );
+
+                  authToken.setDetails(
+                          new WebAuthenticationDetailsSource().buildDetails(request)
+                  );
+
+                  SecurityContextHolder.getContext().setAuthentication(authToken);
+              }
+          }
+      }
+
+      filterChain.doFilter(request, response);
   }
 }

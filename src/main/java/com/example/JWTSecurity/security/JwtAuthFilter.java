f8bc3e7ff50ebeb31cab3f8fe3a196b4cc1d4c56
Mahesh-Khadka <maheshkhadka438@gmail.com>
2025-09-23 20:20:53 +0545

    feat(JwtAuthFilter): extract JWT from Authorization header and extract email from token in JwtAuthFilter.

--- src/main/java/com/example/JWTSecurity/security/JwtAuthFilter.java
+++ src/main/java/com/example/JWTSecurity/security/JwtAuthFilter.java
@@ -1,7 +1,13 @@
 import com.example.JWTSecurity.repository.UserRepository;
 import com.example.JWTSecurity.security.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
   private final JwtUtil jwtUtil;
@@ -10,5 +16,13 @@ public class JwtAuthFilter extends OncePerRequestFilter {
   public JwtAuthFilter(JwtUtil jwtUtil, UserRepository userRepository){
       this.jwtUtil = jwtUtil;
       this.userRepository = userRepository;
+  }
+
+  @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws SecurityException, IOException{
+      final String authHeader = request.getHeader("Authorization");
+      String email = null;
+      String token = null;
   }
 }
